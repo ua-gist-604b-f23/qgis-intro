be77771c0f90f5bd640c1732015d98a5864daa79
Aaryn Olsson <aaryn@email.arizona.edu>
2019-10-27 14:40:37 -0700

    Update check_files.sh

--- .travis/check_files.sh
+++ .travis/check_files.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 ret=0
-cd "$(dirname "$0")"
-if [ ! -f required_files.txt ]; then
-    echo "required_files.txt not found"
+dir=$(dirname "$0")
+if [ ! -f $dir/required_files.txt ]; then
+    echo "dir/required_files.txt not found"
     exit 1
 fi
-for f in $(cat required_files.txt); do 
+for f in $(cat $dir/required_files.txt); do 
   if [ ! -r $f ]; then 
-     >&2 echo "file $f does not exist or is not readable"
+     echo "file $f does not exist or is not readable"
      ret=1
+  else
+    echo "$f exists:"
+    ls -al $f
   fi
 done
 exit $ret
